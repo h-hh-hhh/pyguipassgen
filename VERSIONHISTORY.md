6102d5368715a0b7e33bb92d252b48b2d371b552
h-hh-hhh <t3hwhistler2419@gmail.com>
2020-06-10 02:46:42 +0300

    (0.1.0) changed gui
    GUI changed to PySimpleGUI
    version 0.1.0

--- VERSIONHISTORY.md
+++ VERSIONHISTORY.md
@@ -1,3 +1,7 @@
+# 0.1.0
+
+- Switched GUI to PySimpleGUI (old tkinter version still left for tjis commit only)
+
 # 0.0.1
 
 - First version
--- main.py
+++ main.py
@@ -1,4 +1,20 @@
 import tkinter, random
+import PySimpleGUI as sg
+
+layout = [[sg.Text('Enter characters to be included in the password'),sg.Input('abcdefhijklmnopqrstuvwxyzABCDEFHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&')],[sg.Text('Generated password:'),sg.Input(key='_GENERATED_')],[sg.Button('Generate')]]
+
+window = sg.Window('Password Generator', layout)
+
+while True:  # Event Loop
+    event, values = window.read()
+    print(event, values)
+    if event == sg.WIN_CLOSED:
+        break
+    if event == 'Generate':
+        values[1] = ''.join(random.choice(values[0]) for i in range(8))
+        window.Refresh()
+        window.FindElement('_GENERATED_').Update(values[1])
+"""
 class simpleapp_tk(tkinter.Tk):
     def __init__(self,parent):
         tkinter.Tk.__init__(self,parent)
@@ -33,3 +49,4 @@ if __name__ == "__main__":
     app = simpleapp_tk(None)
     app.title('Password Generator')
     app.mainloop()
+"""
